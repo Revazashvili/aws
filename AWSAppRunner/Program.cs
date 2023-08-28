4522e1eb4be7ce7cbaf9b132ab34dc54551cca6c
Revazashvili <levanrevazashvili@gmail.com>
2023-08-28 18:44:10 +0400

    add health checks

--- AWSAppRunner/Program.cs
+++ AWSAppRunner/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseHealthChecks("/health");
 
 app.Run();
