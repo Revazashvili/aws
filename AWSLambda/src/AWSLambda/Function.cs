3a63f60dce878db1b0a82821680dc85efa680e61
Revazashvili <levanrevazashvili@gmail.com>
2023-09-02 21:57:29 +0400

    use types to use with proxy gateway

--- AWSLambda/src/AWSLambda/Function.cs
+++ AWSLambda/src/AWSLambda/Function.cs
@@ -1,22 +1,35 @@
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace AWSLambda
+namespace AWSLambda;
+
+public class Function
 {
-    public class Function
+    [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        if (request is null)
         {
-            context.Logger.Log($"Got new message: {input}");
-            return input?.ToUpper();
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 500,
+                Body = "request is null"
+            };
         }
+        var name = request.QueryStringParameters is not null && 
+                   request.QueryStringParameters.TryGetValue("name", out var value) ? value : "No Name";
+        context.Logger.Log($"Got Name: {name}");
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = $"User name passed: {name}",
+            Headers = new Dictionary<string, string>(),
+            IsBase64Encoded = false,
+            MultiValueHeaders = new Dictionary<string, IList<string>>()
+        };
     }
 }
